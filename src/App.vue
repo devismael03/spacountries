e5f619eeb669b3ef89f18fb12ea27108276c9083
ocius <mamedzade030@gmail.com>
2017-08-17 17:46:54 +0400

    Fetched data from country list api

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <template v-for="country in countries">
+      <h1>hello</h1>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      countries:[]
+    }
+  },
+  created(){
+    fetch("https://restcountries.eu/rest/v2/all").then((res) => {
+      return res.json(); }).then((res) => {
+        this.countries=res; });
+      }
+
+  }
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+</style>
